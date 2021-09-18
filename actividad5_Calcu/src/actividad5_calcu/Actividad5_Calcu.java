d56558e5ddc210e27a893b1abd14d28625188f66
DrGrodus <ebermd00@gmail.com>
2021-09-18 15:20:14 -0500

    construccion de codigo

--- actividad5_Calcu/src/actividad5_calcu/Actividad5_Calcu.java
+++ actividad5_Calcu/src/actividad5_calcu/Actividad5_Calcu.java
@@ -1,21 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package actividad5_calcu;
-
 /**
  *
  * @author Eber MD
  */
 public class Actividad5_Calcu {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        /* El programa hará operaciones aritmétcias básicas 
+        (suma, resta, multiplicación, división y módulo)
+        */
+        int num1 = 6;
+        int num2 = 8;// Numeros a evaluar
+        
+        int suma = num1 + num2; // sumatoria
+        int resta = num1 - num2; // resta
+        double multi = num1 * num2; // multiplicación
+        double div = num1 / num2; // división
+        double mod = num1 % num2; // módulo
+        
+        System.out.println("Los números son: " + num1 + " y " + num2 
+        + "\n" + "La suma da como resultado: "              + suma
+        + "\n" + "La resta da como resultado: "             + resta
+        + "\n" + "La multiplicación da como resultado: "    + multi
+        + "\n" + "La div da como resultado: "               + div
+        + "\n" + "La módulo da como resultado: "            + mod);
     }
-    
 }
