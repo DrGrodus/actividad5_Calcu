a26a6fb1d10697f9d48f8e2f47e0ecf4d44ef991
DrGrodus <ebermd00@gmail.com>
2021-09-18 13:19:54 -0500

    Proyecto agregado

---
+++ actividad5_Calcu/src/actividad5_calcu/Actividad5_Calcu.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actividad5_calcu;
+
+/**
+ *
+ * @author Eber MD
+ */
+public class Actividad5_Calcu {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
